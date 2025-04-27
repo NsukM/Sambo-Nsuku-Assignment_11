58472d281a5fda0cba71498e14e20f96c907b8d8
NsukM <105019575+NsukM@users.noreply.github.com>
2025-04-27 20:43:17 +0200

    Update repository.py

--- repositories/repository.py
+++ repositories/repository.py
@@ -1,1 +1,22 @@
+from typing import Generic, TypeVar, Optional, List
+from abc import ABC, abstractmethod
 
+T = TypeVar('T')
+ID = TypeVar('ID')
+
+class Repository(ABC, Generic[T, ID]):
+    @abstractmethod
+    def save(self, entity: T) -> None:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, id: ID) -> Optional[T]:
+        pass
+
+    @abstractmethod
+    def find_all(self) -> List[T]:
+        pass
+
+    @abstractmethod
+    def delete(self, id: ID) -> None:
+        pass
